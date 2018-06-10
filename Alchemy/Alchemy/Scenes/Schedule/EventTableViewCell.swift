fb75bfbd0c47eab0cdce97a82a3cdf4185b03432
Unknown <etbrady@outlook.com>
2018-06-10 18:32:41 -0500

    Style event cells

--- Alchemy/Alchemy/Scenes/Schedule/EventTableViewCell.swift
+++ Alchemy/Alchemy/Scenes/Schedule/EventTableViewCell.swift
@@ -2,14 +2,76 @@ import Foundation
 import UIKit
 import SnapKit
 
-class EventTableViewCell: UITableViewCell, Configurable, Reusable {
-    typealias Model = Event
+class EventTableViewCell: UITableViewCell, Reusable {
+    let verticalStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        stackView.alignment = .fill
+        stackView.distribution = .fillEqually
+        
+        return stackView
+    }()
+    
+    let topRowStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .horizontal
+        stackView.alignment = .fill
+        stackView.distribution = .fillEqually
+        
+        return stackView
+    }()
+    
+    let bottomRowStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .horizontal
+        stackView.alignment = .fill
+        stackView.distribution = .fillEqually
+        
+        return stackView
+    }()
     
     let nameLabel: UILabel = {
         let label = UILabel()
-        label.numberOfLines = 0
+        label.textColor = .alchemyBlue
+        label.font = .systemFont(ofSize: 22)
+        label.adjustsFontSizeToFitWidth = true
+        label.minimumScaleFactor = 0.5
+
         return label
     }()
+    
+    let timeLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .alchemyDarkGrey
+        label.adjustsFontSizeToFitWidth = true
+        label.minimumScaleFactor = 0.5
+
+        return label
+    }()
+    
+    let instructorLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .alchemyDarkGrey
+        label.textAlignment = .right
+        label.adjustsFontSizeToFitWidth = true
+        label.minimumScaleFactor = 0.5
+
+        return label
+    }()
+    
+    let locationLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .alchemyLightGrey
+        label.textAlignment = .right
+        label.adjustsFontSizeToFitWidth = true
+        label.minimumScaleFactor = 0.5
+
+        return label
+    }()
+    
     
     override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
@@ -20,20 +82,51 @@ class EventTableViewCell: UITableViewCell, Configurable, Reusable {
         fatalError("init(coder:) has not been implemented")
     }
     
-    func configure(with model: Event) {
-        nameLabel.text = model.name
-    }
-    
     private func setupViews() {
-        self.addSubview(nameLabel)
+        topRowStackView.addArrangedSubview(nameLabel)
+        topRowStackView.addArrangedSubview(instructorLabel)
         
-        nameLabel.snp.makeConstraints({ make in
-            make.leading.equalTo(self).offset(16)
-            make.centerY.equalTo(self)
-            make.width.equalTo(self).inset(16)
-        })
+        bottomRowStackView.addArrangedSubview(timeLabel)
+        bottomRowStackView.addArrangedSubview(locationLabel)
+        
+        verticalStackView.addArrangedSubview(topRowStackView)
+        verticalStackView.addArrangedSubview(bottomRowStackView)
+        
+        self.addSubview(verticalStackView)
+        verticalStackView.snp.makeConstraints(({ make in
+            make.center.equalTo(self)
+            make.size.equalTo(self).inset(16)
+        }))
         
         self.contentView.layoutIfNeeded()
     }
+}
+
+extension EventTableViewCell: Configurable {
+    typealias Model = Event
     
+    func configure(with model: Event) {
+        nameLabel.text = model.name
+        timeLabel.text = getTime(from: model)
+        instructorLabel.text = model.instructor
+        locationLabel.text = model.location?.rawValue
+    }
+    
+    private func getTime(from event: Event) -> String {
+        guard let startDate = event.startDate
+            , let endDate = event.endDate else {
+                return ""
+        }
+        let startDateString = format(startDate)
+        let endDateString = format(endDate)
+        
+        return "\(startDateString) - \(endDateString)"
+    }
+    
+    private func format(_ date: Date) -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "h:mm a"
+        
+        return dateFormatter.string(from: date)
+    }
 }
